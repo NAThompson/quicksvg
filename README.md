24cfc2fd051e4346055610da6bc187d9b91f2d32
Nick Thompson <nathompson7@protonmail.com>
2018-10-28 13:53:53 -0600

    Add SVG to README.md

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # quicksvg
 
 This is a tiny library for generating SVG files from within C++. It plots time series, and graphs functions, and that's it. If you use it, you should think "huh, that kinda did what I wanted", and accept that the result is good enough, or you should give up immediately.
+
+Here's an example of a function graphed:
+
+![Alt text](./examples/1F1_3.svg)
+<img src="./examples/1F1_3.svg">
+
+Here's an example of a time series:
+
+![Alt text](./examples/sin_cos_time_series.svg)
+<img src="./examples/sin_cos_time_series.svg">
--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main() {
     double a = -3;
     double b = 2;
     std::string title = "sin(𝑥)";
-    std::string filename = "sine.svg";
+    std::string filename = "examples/sine.svg";
     auto f = [](double x)->double { return std::sin(x); };
     plot_fn(f, a, b, title, filename);
 
@@ -22,14 +22,14 @@ int main() {
     a = -3;
     b = 2;
     title = "\u2081F\u2081(3, 7, 𝑥)";
-    filename = "1F1_1.svg";
+    filename = "examples/1F1_1.svg";
     plot_fn(onef1_1, a, b, title, filename);
 
     auto onef1_2 = [](double x)->double { return hypergeometric_1F1(-2, 3, x); };
     a = -3;
     b = 2;
     title = "\u2081F\u2081(-2, 3, 𝑥)";
-    filename = "1F1_2.svg";
+    filename = "examples/1F1_2.svg";
     plot_fn(onef1_2, a, b, title, filename);
 
 
@@ -37,7 +37,7 @@ int main() {
     a = -2;
     b = 1;
     title = "\u2081F\u2081(2, -2.5, 𝑥)";
-    filename = "1F1_3.svg";
+    filename = "examples/1F1_3.svg";
     plot_fn(onef1_3, a, b, title, filename);
 
 
@@ -45,7 +45,7 @@ int main() {
     a = -4;
     b = 1;
     title = "\u2081F\u2081(-2, -2.5, 𝑥)";
-    filename = "1F1_4.svg";
+    filename = "examples/1F1_4.svg";
     plot_fn(onef1_4, a, b, title, filename);
 
 
@@ -58,8 +58,8 @@ int main() {
       u[i] = std::cos(start_time + i*time_step);
     }
 
-    title = "sine time series";
-    filename = "examples/sine_time_series.svg";
+    title = "sine and cosine time series";
+    filename = "examples/sin_cos_time_series.svg";
 
     quicksvg::plot_time_series pts(start_time, time_step, title, filename);
     pts.add_dataset(v);
