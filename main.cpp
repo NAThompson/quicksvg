f5cd0b9dfb9f24dd183b1726ed8075bf58d0c89d
Nick Thompson <nathompson7@protonmail.com>
2018-10-28 10:55:15 -0600

    Gridlines with labelled values.

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main() {
 
     double a = -3;
     double b = 2;
-    std::string title = "sin(x)";
+    std::string title = "sin(𝑥)";
     std::string filename = "sine.svg";
     auto f = [](double x)->double { return std::sin(x); };
     plot_fn(f, a, b, title, filename);
@@ -19,30 +19,30 @@ int main() {
     auto onef1_1 = [](double x)->double { return hypergeometric_1F1(3, 7, x); };
     a = -3;
     b = 2;
-    title = "\u2081F\u2081(3, 7, x)";
+    title = "\u2081F\u2081(3, 7, 𝑥)";
     filename = "1F1_1.svg";
     plot_fn(onef1_1, a, b, title, filename);
 
     auto onef1_2 = [](double x)->double { return hypergeometric_1F1(-2, 3, x); };
     a = -3;
     b = 2;
-    title = "\u2081F\u2081(-2, 3, x)";
+    title = "\u2081F\u2081(-2, 3, 𝑥)";
     filename = "1F1_2.svg";
     plot_fn(onef1_2, a, b, title, filename);
 
 
     auto onef1_3 = [](double x)->double { return hypergeometric_1F1(2, -2.5, x); };
-    a = 0;
-    b = 3;
-    title = "\u2081F\u2081(2, -2.5, x)";
+    a = -2;
+    b = 1;
+    title = "\u2081F\u2081(2, -2.5, 𝑥)";
     filename = "1F1_3.svg";
     plot_fn(onef1_3, a, b, title, filename);
 
 
     auto onef1_4 = [](double x)->double { return hypergeometric_1F1(-2, -2.5, x); };
-    a = -3;
-    b = 2;
-    title = "\u2081F\u2081(-2, -2.5, x)";
+    a = -4;
+    b = 1;
+    title = "\u2081F\u2081(-2, -2.5, 𝑥)";
     filename = "1F1_4.svg";
     plot_fn(onef1_4, a, b, title, filename);
 
--- plot_fn.hpp
+++ plot_fn.hpp
@@ -1,6 +1,7 @@
 #ifndef PLOT_FN_HPP
 #define PLOT_FN_HPP
 
+#include <iomanip>
 #include <cassert>
 #include <vector>
 #include <utility>
@@ -50,8 +51,8 @@ void plot_fn(F f, double a, double b, std::string const & title, std::string con
       return ((max_val - y)/(max_val - min_val) )*static_cast<double>(graph_height);
     };
 
-    fs << "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n"
-       << "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width='"
+    fs << "<?xml version=\"1.0\" encoding='UTF-8' ?>\n"
+       << "<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='"
        << width << "' height='"
        << height << "'>\n"
        // Black background; I don't want to go blind:
@@ -62,18 +63,7 @@ void plot_fn(F f, double a, double b, std::string const & title, std::string con
        << "' y='25' font-family='times' font-size='25' fill='white'>"
        << title
        << "</text>\n"
-       // Label a:
-       << "<text x='" << margin_left
-       << "' y='" << height - margin_bottom/6
-       << "' font-family='times' font-size='20' fill='white'>"
-       << a
-       << "</text>\n"
-       // Label b:
-       << "<text x='" << margin_left + graph_width -5
-       << "' y='" << height - margin_bottom/6
-       << "' font-family='times' font-size='20' fill='white'>"
-       << b
-       << "</text>\n"
+
 
        // Construct SVG group to simplify the calculations slightly:
        << "<g transform='translate(" << margin_left << ", " << margin_top << ")'>\n"
@@ -83,14 +73,51 @@ void plot_fn(F f, double a, double b, std::string const & title, std::string con
        // x-axis: If 0 is between the min a max height, place the axis at zero.
        // Otherwise, place is at the bottom of the graph.
     double x_axis_loc = graph_height;
+    double x_axis_yval = min_val;
     if (min_val <= 0 && max_val >= 0) {
          x_axis_loc = yScale(0);
+         x_axis_yval = 0;
     }
     fs << "<line x1='0' y1='" << x_axis_loc << "' x2='" << graph_width << "' y2='" << x_axis_loc
-       << "' stroke='gray' stroke-width='1' />\n"
+       << "' stroke='gray' stroke-width='1' />\n";
+
+    /*// Label the y-coordinate of the x_axis:
+    fs << "<text x='" <<  -margin_left/2 + 3 << "' y='" << x_axis_loc - 3
+       << "' font-family='times' font-size='10' fill='white' transform='rotate(-90 "
+       << -margin_left/2 + 11 << " " << x_axis_loc + 5 << ")'>"
+       <<  std::fixed << std::setprecision(2) << x_axis_yval << "</text>\n";*/
+
+    // Make a grid:
+    int horizontal_lines = 8;
+    for (int i = 1; i <= horizontal_lines; ++i) {
+        double y_cord_dataspace = min_val +  ((max_val - min_val)*i)/horizontal_lines;
+        double y = yScale(y_cord_dataspace);
+        fs << "<line x1='0' y1='" << y << "' x2='" << graph_width
+           << "' y2='" << y
+           << "' stroke='gray' stroke-width='1' opacity='0.5' stroke-dasharray='4' />\n";
+
+        fs << "<text x='" <<  -margin_left/2 + 5 << "' y='" << y - 3
+           << "' font-family='times' font-size='10' fill='white' transform='rotate(-90 "
+           << -margin_left/2 + 11 << " " << y + 5 << ")'>"
+           <<  std::fixed << std::setprecision(2) << y_cord_dataspace << "</text>\n";
+     }
+
+     int vertical_lines = 10;
+     for (int i = 1; i <= vertical_lines; ++i) {
+         double x_cord_dataspace = a +  ((b - a)*i)/vertical_lines;
+         double x = xScale(x_cord_dataspace);
+         fs << "<line x1='" << x << "' y1='0' x2='" << x
+            << "' y2='" << graph_height
+            << "' stroke='gray' stroke-width='1' opacity='0.5' stroke-dasharray='4' />\n";
+
+          fs << "<text x='" <<  x - 10  << "' y='" << graph_height + 10
+               << "' font-family='times' font-size='10' fill='white'>"
+               <<  std::fixed << std::setprecision(2) << x_cord_dataspace << "</text>\n";
+
+      }
 
 
-       << "<path d='M" << xScale(xys[0].first) << " " << yScale(xys[0].second);
+    fs  << "<path d='M" << xScale(xys[0].first) << " " << yScale(xys[0].second);
     for (size_t i = 1; i < xys.size(); ++i) {
         // Yes, this is linear interpolation.
         // I'm not gonna bother with Bezier curve control point computation.
